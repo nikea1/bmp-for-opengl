7f1b5763a355057ce0cb429aeadd82fe91c0146b
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2018-02-14 20:08:05 -0500

    Initial Commit

---
+++ bmp-for-opengl/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  bmp-for-opengl
+//
+//  Created by Adenike Adeyemi on 2/14/18.
+//  Copyright © 2018 Adenike Adeyemi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
